6bf5aa843043ba3ffc278461bacb5b606ee2846f
Javey <zoujiawei@baidu.com>
2014-03-25 01:29:05 +0800

    打包脚本更新

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @fileoverview M3D入口
+ * @author zoujiawei@baidu.com
+ * 13-3-20 下午2:03
+ * Created by JetBrains PhpStorm.
+ *
+ * 每位工程师都有保持代码优雅的义务
+ * each engineer has a duty to keep the code elegant
+ */
+
+defined('PROJECT_PATH') or define('PROJECT_PATH', dirname($_SERVER['SCRIPT_FILENAME']));
+//defined('M3D_CORE_PATH') or define('M3D_CORE_PATH', dirname(__FILE__));
+// symlink model for debug
+defined('M3D_CORE_PATH') or define('M3D_CORE_PATH', dirname(dirname(PROJECT_PATH)).'/m3d');
+defined('M3D_PATH') or define('M3D_PATH', dirname(M3D_CORE_PATH));
+
+require M3D_CORE_PATH . '/Common/common.php';
--- m3d.php
+++ m3d.php
@@ -1,18 +1,54 @@
 <?php
 /**
- * @fileoverview M3D入口
- * @author zoujiawei@baidu.com
- * 13-3-20 下午2:03
- * Created by JetBrains PhpStorm.
- *
- * 每位工程师都有保持代码优雅的义务
- * each engineer has a duty to keep the code elegant
+ * Created by JetBrains PhpStorm. M3D安装脚本
+ * User: zoujiawei
+ * Date: 14-3-21
+ * Time: 下午9:50
+ * To change this template use File | Settings | File Templates.
  */
 
-defined('PROJECT_PATH') or define('PROJECT_PATH', dirname($_SERVER['SCRIPT_FILENAME']));
-//defined('M3D_CORE_PATH') or define('M3D_CORE_PATH', dirname(__FILE__));
-// symlink model for debug
-defined('M3D_CORE_PATH') or define('M3D_CORE_PATH', dirname(dirname(PROJECT_PATH)).'/m3d');
-defined('M3D_PATH') or define('M3D_PATH', dirname(M3D_CORE_PATH));
+if (!isset($argv[1])) {
+    exit("请传入工程名\n");
+}
 
-require M3D_CORE_PATH . '/Common/common.php';
+$projectName = $argv[1];
+//$curPath = dirname(__FILE__);
+exec('pwd', $info);
+$curPath = $info[0];
+
+$curDir = basename($curPath);
+$parentPath = dirname($curPath);
+$projectPath = $parentPath.'/project/'.$projectName;
+
+//if (file_exists($projectPath)) {
+//    exit("该工程已存在\n");
+//}
+
+// 如果是第一次安装，则需要初始化系统
+if (!file_exists("${parentPath}/conf/config.php")) {
+    shell_exec("cp -rf ${curPath}/Install/global/* ${parentPath}");
+}
+
+mkdir($projectPath, 0777, true);
+shell_exec("cp -rf ${curPath}/Install/project/* ${projectPath}");
+
+// 修改index.php
+$indexPath = $projectPath.'/index.php';
+$contents = file_get_contents($indexPath);
+$contents = str_replace('{M3D_CORE}', $curDir, $contents);
+file_put_contents($indexPath, $contents);
+
+// 建立静态文件软链
+shell_exec("ln -snf ${curPath}/Ui/static ${projectPath}/static");
+
+// 修改lighttpd配置文件
+$lighttpdConfPath = $projectPath.'/lighttpd.conf';
+$contents = file_get_contents($lighttpdConfPath);
+$contents = str_replace(
+    array('{project_name}', '{project_path}'),
+    array($projectName, $projectPath),
+    $contents
+);
+file_put_contents($lighttpdConfPath, $contents);
+
+echo "安装完成\nvHost:${projectName}.m3d.com\nHave fun！\n";
