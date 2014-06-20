eb7f8feb93b5b08493d68ef0fa414584c334f149
Javey <zoujiawei@baidu.com>
2014-06-20 17:52:00 +0800

    测试环境地址加入端口参数

--- Install/project/conf/config.php
+++ Install/project/conf/config.php
@@ -7,6 +7,7 @@ return array(
     'PROJECT' => array(
         'INFO' => array(
             'host' => 'music.baidu.com',
+            'port' => '80',
             'name' => '百度音乐主站'
         )
     )
